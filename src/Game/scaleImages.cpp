fa2cddc9557bd5272b60b0b3594d53abb9d88d22
jheinz1999 <jonathan.heinz@aol.com>
2016-01-24 17:44:41 -0800

    finished map at res 1024x768

--- src/Game/scaleImages.cpp
+++ src/Game/scaleImages.cpp
@@ -27,6 +27,30 @@ gameMap.getMapPiece("egypt")->scale(width, height);
 gameMap.getMapPiece("senegal")->scale(width, height);
 gameMap.getMapPiece("sudan")->scale(width, height);
 gameMap.getMapPiece("guinea")->scale(width, height);
+gameMap.getMapPiece("ethiopia")->scale(width, height);
+gameMap.getMapPiece("nigeria")->scale(width, height);
+gameMap.getMapPiece("morocco")->scale(width, height);
+gameMap.getMapPiece("western sahara")->scale(width, height);
+gameMap.getMapPiece("tunisia")->scale(width, height);
+gameMap.getMapPiece("somalia")->scale(width, height);
+gameMap.getMapPiece("ghana")->scale(width, height);
+gameMap.getMapPiece("liberia")->scale(width, height);
+gameMap.getMapPiece("sierra leone")->scale(width, height);
+gameMap.getMapPiece("uganda")->scale(width, height);
+gameMap.getMapPiece("central republic")->scale(width, height);
+gameMap.getMapPiece("cameroon")->scale(width, height);
+gameMap.getMapPiece("congo")->scale(width, height);
+gameMap.getMapPiece("gabon")->scale(width, height);
+gameMap.getMapPiece("kenya")->scale(width, height);
+gameMap.getMapPiece("democratic congo")->scale(width, height);
+gameMap.getMapPiece("namibia")->scale(width, height);
+gameMap.getMapPiece("angola")->scale(width, height);
+gameMap.getMapPiece("tanzania")->scale(width, height);
+gameMap.getMapPiece("mozambique")->scale(width, height);
+gameMap.getMapPiece("zambia")->scale(width, height);
+gameMap.getMapPiece("botswana")->scale(width, height);
+gameMap.getMapPiece("south africa")->scale(width, height);
+gameMap.getMapPiece("zimbabwe")->scale(width, height);
 
 entityManager.getEntity("map")->setPosition(sf::Vector2f(0, 0));
 entityManager.getEntity("map")->getSprite()->setOrigin(0, 0);
@@ -47,15 +71,31 @@ entityManager.getEntity("bg")->getSprite()->setOrigin(0, 0);
 	gameMap.getMapPiece("senegal")->getSprite()->setPosition(sf::Vector2f(32, 240));
 	gameMap.getMapPiece("sudan")->getSprite()->setPosition(sf::Vector2f(510, 250));
 	gameMap.getMapPiece("guinea")->getSprite()->setPosition(sf::Vector2f(65, 289));
-
-	}
-
-	if (screen.width == 1024 && screen.height == 744) {
-
-	gameMap.getMapPiece("mauritania")->getSprite()->setPosition(sf::Vector2f(78, 166));
-	gameMap.getMapPiece("algeria")->getSprite()->setPosition(sf::Vector2f(214, 94));
-	gameMap.getMapPiece("mali")->getSprite()->setPosition(sf::Vector2f(140, 195));
-
+	gameMap.getMapPiece("ethiopia")->getSprite()->setPosition(sf::Vector2f(630, 281));
+	gameMap.getMapPiece("morocco")->getSprite()->setPosition(sf::Vector2f(139, 64));
+	gameMap.getMapPiece("nigeria")->getSprite()->setPosition(sf::Vector2f(279, 296));
+	gameMap.getMapPiece("western sahara")->getSprite()->setPosition(sf::Vector2f(70, 139));
+	gameMap.getMapPiece("tunisia")->getSprite()->setPosition(sf::Vector2f(309, 43));
+	gameMap.getMapPiece("somalia")->getSprite()->setPosition(sf::Vector2f(698, 338));
+	gameMap.getMapPiece("ghana")->getSprite()->setPosition(sf::Vector2f(172, 307));
+	gameMap.getMapPiece("liberia")->getSprite()->setPosition(sf::Vector2f(84, 324));
+	gameMap.getMapPiece("sierra leone")->getSprite()->setPosition(sf::Vector2f(58, 304));
+	gameMap.getMapPiece("cameroon")->getSprite()->setPosition(sf::Vector2f(323, 313));
+	gameMap.getMapPiece("uganda")->getSprite()->setPosition(sf::Vector2f(545, 380));
+	gameMap.getMapPiece("central republic")->getSprite()->setPosition(sf::Vector2f(411, 326));
+	gameMap.getMapPiece("congo")->getSprite()->setPosition(sf::Vector2f(348, 398));
+	gameMap.getMapPiece("gabon")->getSprite()->setPosition(sf::Vector2f(315, 399));
+	gameMap.getMapPiece("kenya")->getSprite()->setPosition(sf::Vector2f(606, 392));
+	gameMap.getMapPiece("democratic congo")->getSprite()->setPosition(sf::Vector2f(418, 430));
+	gameMap.getMapPiece("angola")->getSprite()->setPosition(sf::Vector2f(381, 509));
+	gameMap.getMapPiece("namibia")->getSprite()->setPosition(sf::Vector2f(384, 627));
+	gameMap.getMapPiece("tanzania")->getSprite()->setPosition(sf::Vector2f(568, 458));
+	gameMap.getMapPiece("zambia")->getSprite()->setPosition(sf::Vector2f(491, 528));
+	gameMap.getMapPiece("mozambique")->getSprite()->setPosition(sf::Vector2f(574, 586));
+	gameMap.getMapPiece("zimbabwe")->getSprite()->setPosition(sf::Vector2f(505, 591));
+	gameMap.getMapPiece("botswana")->getSprite()->setPosition(sf::Vector2f(452, 624));
+	gameMap.getMapPiece("south africa")->getSprite()->setPosition(sf::Vector2f(449, 689));
+	
 	}
 
 }
--- src/PlayingState/update.cpp
+++ src/PlayingState/update.cpp
@@ -9,6 +9,8 @@
 
 void PlayingState::update() {
 
+Game::entityManager.getEntity("map")->getSprite()->setColor(sf::Color(255, 255, 255, 0));
+
 setSpriteColors();
 
 	if (Game::gameMap.getMapPiece("algeria")->containsMouse()) {
@@ -75,12 +77,6 @@ setSpriteColors();
 
 	Game::gameMap.getMapPiece("sudan")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
 
-		if (Game::isMouseDown) {
-
-		Game::gameMap.getMapPiece("sudan")->setPosition((sf::Vector2f)sf::Mouse::getPosition());
-
-		}
-
 	}
 
 	else if (Game::gameMap.getMapPiece("guinea")->containsMouse()) {
@@ -89,11 +85,167 @@ setSpriteColors();
 
 	}
 
+	else if (Game::gameMap.getMapPiece("morocco")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("morocco")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("nigeria")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("nigeria")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("ethiopia")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("ethiopia")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("somalia")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("somalia")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("tunisia")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("tunisia")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("western sahara")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("western sahara")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("ghana")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("ghana")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("liberia")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("liberia")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("sierra leone")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("sierra leone")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("cameroon")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("cameroon")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("uganda")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("uganda")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("central republic")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("central republic")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("congo")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("congo")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("gabon")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("gabon")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("kenya")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("kenya")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("democratic congo")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("democratic congo")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("angola")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("angola")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("namibia")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("namibia")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("tanzania")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("tanzania")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("zambia")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("zambia")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("mozambique")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("mozambique")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("zimbabwe")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("zimbabwe")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("botswana")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("botswana")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
+	else if (Game::gameMap.getMapPiece("south africa")->containsMouse()) {
+
+	Game::gameMap.getMapPiece("south africa")->getSprite()->setColor(sf::Color(0, 255, 0, 255));
+
+	}
+
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
 	Game::entityManager.getEntity("map")->getSprite()->setColor(sf::Color(255, 255, 255, 0));
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 	Game::entityManager.getEntity("map")->getSprite()->setColor(sf::Color(255, 255, 255, 255));
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::G)) {
+
+	std::cout << std::endl;
+
+	std::cout << Game::gameMap.getMapPiece("zimbabwe")->getPosition().x << ", " << Game::gameMap.getMapPiece("zimbabwe")->getPosition().y << std::endl;
+	std::cout << Game::gameMap.getMapPiece("botswana")->getPosition().x << ", " << Game::gameMap.getMapPiece("botswana")->getPosition().y << std::endl;
+	std::cout << Game::gameMap.getMapPiece("south africa")->getPosition().x << ", " << Game::gameMap.getMapPiece("south africa")->getPosition().y << std::endl;
+
+	std::cout << std::endl;
+
+	}
 
 } 
 
@@ -111,5 +263,29 @@ Game::gameMap.getMapPiece("sudan")->getSprite()->setColor(Game::gameMap.getMapPi
 Game::gameMap.getMapPiece("libya")->getSprite()->setColor(Game::gameMap.getMapPiece("libya")->getColor());
 Game::gameMap.getMapPiece("niger")->getSprite()->setColor(Game::gameMap.getMapPiece("niger")->getColor());
 Game::gameMap.getMapPiece("guinea")->getSprite()->setColor(Game::gameMap.getMapPiece("guinea")->getColor());
+Game::gameMap.getMapPiece("nigeria")->getSprite()->setColor(Game::gameMap.getMapPiece("nigeria")->getColor());
+Game::gameMap.getMapPiece("morocco")->getSprite()->setColor(Game::gameMap.getMapPiece("morocco")->getColor());
+Game::gameMap.getMapPiece("ethiopia")->getSprite()->setColor(Game::gameMap.getMapPiece("ethiopia")->getColor());
+Game::gameMap.getMapPiece("western sahara")->getSprite()->setColor(Game::gameMap.getMapPiece("western sahara")->getColor());
+Game::gameMap.getMapPiece("tunisia")->getSprite()->setColor(Game::gameMap.getMapPiece("tunisia")->getColor());
+Game::gameMap.getMapPiece("somalia")->getSprite()->setColor(Game::gameMap.getMapPiece("somalia")->getColor());
+Game::gameMap.getMapPiece("ghana")->getSprite()->setColor(Game::gameMap.getMapPiece("ghana")->getColor());
+Game::gameMap.getMapPiece("liberia")->getSprite()->setColor(Game::gameMap.getMapPiece("liberia")->getColor());
+Game::gameMap.getMapPiece("sierra leone")->getSprite()->setColor(Game::gameMap.getMapPiece("sierra leone")->getColor());
+Game::gameMap.getMapPiece("cameroon")->getSprite()->setColor(Game::gameMap.getMapPiece("cameroon")->getColor());
+Game::gameMap.getMapPiece("uganda")->getSprite()->setColor(Game::gameMap.getMapPiece("uganda")->getColor());
+Game::gameMap.getMapPiece("central republic")->getSprite()->setColor(Game::gameMap.getMapPiece("central republic")->getColor());
+Game::gameMap.getMapPiece("congo")->getSprite()->setColor(Game::gameMap.getMapPiece("congo")->getColor());
+Game::gameMap.getMapPiece("gabon")->getSprite()->setColor(Game::gameMap.getMapPiece("gabon")->getColor());
+Game::gameMap.getMapPiece("kenya")->getSprite()->setColor(Game::gameMap.getMapPiece("kenya")->getColor());
+Game::gameMap.getMapPiece("democratic congo")->getSprite()->setColor(Game::gameMap.getMapPiece("democratic congo")->getColor());
+Game::gameMap.getMapPiece("angola")->getSprite()->setColor(Game::gameMap.getMapPiece("angola")->getColor());
+Game::gameMap.getMapPiece("namibia")->getSprite()->setColor(Game::gameMap.getMapPiece("namibia")->getColor());
+Game::gameMap.getMapPiece("tanzania")->getSprite()->setColor(Game::gameMap.getMapPiece("tanzania")->getColor());
+Game::gameMap.getMapPiece("zambia")->getSprite()->setColor(Game::gameMap.getMapPiece("zambia")->getColor());
+Game::gameMap.getMapPiece("mozambique")->getSprite()->setColor(Game::gameMap.getMapPiece("mozambique")->getColor());
+Game::gameMap.getMapPiece("zimbabwe")->getSprite()->setColor(Game::gameMap.getMapPiece("zimbabwe")->getColor());
+Game::gameMap.getMapPiece("botswana")->getSprite()->setColor(Game::gameMap.getMapPiece("botswana")->getColor());
+Game::gameMap.getMapPiece("south africa")->getSprite()->setColor(Game::gameMap.getMapPiece("south africa")->getColor());
 
 }
