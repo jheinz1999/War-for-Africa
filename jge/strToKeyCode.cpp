37ab424ebc4ae44bd70e5365a9e8c78bcca6f97c
jheinz1999 <jonathan.heinz@aol.com>
2016-02-24 22:34:46 -0800

    extended Engine functionality

--- jge/strToKeyCode.cpp
+++ jge/strToKeyCode.cpp
@@ -6,18 +6,89 @@ namespace jge {
 
 	sf::Keyboard::Key strToKeyCode(std::string key) { // converts std::string to sf::Keyboard::Key
 
-		if (key.length() == 1) {
+		if (key == "ESC")
+		return sf::Keyboard::Escape;
 
+		if (key == "LSHIFT")
+		return sf::Keyboard::LShift;
 
+		if (key == "A")
+		return sf::Keyboard::A;
 
-		}
+		if (key == "B")
+		return sf::Keyboard::B;
 
-		if (key.length() == 3) {
+		if (key == "C")
+		return sf::Keyboard::C;
 
-			if (key == "ESC")
-			return sf::Keyboard::Escape;
+		if (key == "D")
+		return sf::Keyboard::D;
 
-		}
+		if (key == "E")
+		return sf::Keyboard::E;
+
+		if (key == "F")
+		return sf::Keyboard::F;
+
+		if (key == "G")
+		return sf::Keyboard::G;
+
+		if (key == "H")
+		return sf::Keyboard::H;
+
+		if (key == "I")
+		return sf::Keyboard::I;
+
+		if (key == "J")
+		return sf::Keyboard::J;
+
+		if (key == "K")
+		return sf::Keyboard::K;
+
+		if (key == "L")
+		return sf::Keyboard::L;
+
+		if (key == "M")
+		return sf::Keyboard::M;
+
+		if (key == "N")
+		return sf::Keyboard::N;
+
+		if (key == "O")
+		return sf::Keyboard::O;
+
+		if (key == "P")
+		return sf::Keyboard::P;
+
+		if (key == "Q")
+		return sf::Keyboard::Q;
+
+		if (key == "R")
+		return sf::Keyboard::R;
+
+		if (key == "S")
+		return sf::Keyboard::S;
+
+		if (key == "T")
+		return sf::Keyboard::T;
+
+		if (key == "U")
+		return sf::Keyboard::U;
+
+		if (key == "V")
+		return sf::Keyboard::V;
+
+		if (key == "W")
+		return sf::Keyboard::W;
+
+		if (key == "X")
+		return sf::Keyboard::X;
+
+		if (key == "Y")
+		return sf::Keyboard::Y;
+
+		if (key == "Z")
+		return sf::Keyboard::Z;
 
 	}
 
